1b64b98c852a6fca27fd1710fbb68aaf8b1ee801
Luke Modugno <lmodugno5@yahoo.com>
2021-09-14 20:41:32 -0700

    added randomization and font scaling

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var imageNumber = 0
     var messageNumber = 0
+    let totalNumberOfImages = 9
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,22 +25,25 @@ class ViewController: UIViewController {
 
    
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let messages = ["you are awesome!", "you are great!", "you are fantastic!", "awesome? thats you!"]
+        let messages = ["you are awesome!", "you are great!", "you are fantastic!", "awesome? thats you!", "when the genius bar needs help they call you!", "you got the design skills of Kanye West!"]
+      
+        mesageLabel.text = messages[Int.random(in: 0...messages.count-1)]
+        imageViewer.image = UIImage(named: "image\(Int.random(in:0...totalNumberOfImages))")
         
-        mesageLabel.text = messages[messageNumber]
-        messageNumber += 1
-        if messageNumber == messages.count {
-            messageNumber = 0
-            
-        }
+//        mesageLabel.text = messages[messageNumber]
+//        messageNumber += 1
+//        if messageNumber == messages.count {
+//            messageNumber = 0
+//
+//        }
         
         
-        let imageName = "image\(imageNumber)"
-        imageViewer.image = UIImage(named: imageName)
-        imageNumber = imageNumber + 1
-        if imageNumber == 10 {
-            imageNumber = 0
-        }
+//        let imageName = "image\(imageNumber)"
+//        imageViewer.image = UIImage(named: imageName)
+//        imageNumber = imageNumber + 1
+//        if imageNumber == 10 {
+//            imageNumber = 0
+//        }
         
         
 //        let awesomeMessage = "you are awesome!"
